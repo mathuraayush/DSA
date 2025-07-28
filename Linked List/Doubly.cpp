46ab231152cb589832cb91f3fd498d6c2533e59e
Aayush Mathur <mathuraayush15@gmail.com>
2025-07-28 05:14:58 +0000

    Doubly LL Intro Done!

--- Linked List/Doubly.cpp
+++ Linked List/Doubly.cpp
@@ -28,6 +28,41 @@ struct Node
         next=nullptr;
     }
 };
+
+Node* convertArrayToLL(vector<int> a){
+    if(a.empty()) return nullptr;
+    Node* head=new Node(a[0]);
+    Node* current=head;
+    for(int i=1;i<a.size();i++){
+        Node* temp=new Node(a[i]);
+        current->next=temp;
+        temp->prev=current;
+        current=current->next;
+    }
+    return head;
+
+}
+// To print Linked List
+void PrintLL(Node* head){
+    Node* start=head;
+    while(start!=nullptr){
+        cout<<start->data<<" ";
+        start=start->next;
+    }
+    cout<<endl;
+}
+void deleteAtSpecific(Node* head,int target){
+    Node
+    while()
+    Node* temp=tobedeleted->next;
+    tobedeleted->prev->next=temp;
+    delete tobedeleted;
+}
 int main(){
+    vector<int> arr={12,45,89,0,75};
+    Node* start=convertArrayToLL(arr);
+    PrintLL(start);
+    
+
      
 }

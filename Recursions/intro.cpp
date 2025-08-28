165bc606cf7fd72223af952f373d7d48b3a7b852
Aayush Mathur <mathuraayush15@gmail.com>
2025-08-28 04:19:30 +0000

    Done

--- Recursions/intro.cpp
+++ Recursions/intro.cpp
@@ -1,5 +1,6 @@
 // Online C++ compiler to run C++ program online
 #include <iostream>
+#include <vector>
 using namespace std;
 //priniting 1 to N
 void print1toN(int i,int n){
@@ -29,6 +30,40 @@ void printNto1b(int i,int n){
     cout<<i;
     cout<<endl;
 }
+// Sum of First n number
+//Backtracking
+int sumN(int n){
+  if(n<1) return 0;
+  return n+sumN(n-1);
+    }
+//Forward    
+int sumFwd(int n,int i=1){
+    if(i>n) return 0;
+    return i+sumFwd(n,i+1);
+} 
+//Backtrack
+int factorial(int n){
+  if(n<1) return 1;
+  return n*factorial(n-1);
+    } 
+// Forward
+int factorialFwd(int n,int i=1){
+    if(i>n) return 1;
+    return i*factorialFwd(n,i+1);
+}   
+//Swap helper
+void swap(int &a,int &b){
+    int t=a;
+    a=b;
+    b=t;
+}
+// Reverse
+void reverse(vector<int>&a, int n){
+   if(n==a.size()/2) return;
+   reverse(a,n-1);
+   swap(a[n],a[(a.size()-1)-n]);
+}    
+
 int main() {
     // Write C++ code here
     print1toN(1,5);
@@ -38,5 +73,25 @@ int main() {
     printNto1(5);
     cout<<"Backtracking: "<<endl;
     printNto1b(1,5);
-    return 0;
+    cout<<"Sum: "<<endl;
+    int sum=sumN(5);
+    cout<<sum;
+    cout<<endl;
+    cout<<"Sum: "<<endl;
+    int s=sumFwd(5);
+    cout<<s<<endl;
+    cout<<"Factorial: "<<endl;
+    int f=factorial(5);
+    cout<<f<<endl;
+    cout<<"Factorial Fwd: "<<endl;
+    int fwd=factorial(5);
+    cout<<fwd<<endl;
+    cout<<"Original Vector: 1 2 3 4 5"<<endl;
+    vector<int> a={1,2,3,4,5,};
+    cout<<"Reversed Vector: ";
+    reverse(a,a.size());
+    for(auto i:a){
+         cout<<i<<" ";
+    }
+
 }
